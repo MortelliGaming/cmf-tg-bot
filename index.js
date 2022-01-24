d44d316ae11d11323c86bf577f2ba6a2bdaa8163
Dominik Bühler <dominikbuhler@Dominiks-MacBook-Pro.local>
2022-01-24 17:49:40 +0100

    remove unneeded, send ok for all routes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 
-const cron = require('node-cron');
-const axios = require('axios');
 const { setupTGBot } = require('./serverside/telegrambot')
 
 require('dotenv').config()
@@ -9,35 +7,12 @@ const path = __dirname + '/dist/';
 const app = express();
 const secure_app = express();
 
-
-/*
-// keep alive
-cron.schedule('* * * * *', function() {
-    // console.log('running a task every minute');
-    const HEROKU_URL = (process.env.HEROKU_PING_URL || 'https://cmf-ticker-bot.herokuapp.com/')
-    axios.get(HEROKU_URL)
-    .then(function (response) {
-        // handle success
-        console.log('the server is up')
-        // console.log(response);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log('server not found: ', HEROKU_URL);
-    })
-    .then(function () {
-        // always executed
-    });
-});
-// 
-*/ 
-
 app.use(express.static(path));
 app.get('*', function (req,res) {
     res.sendFile(path + "index.html");
 });
 
-secure_app.get('/', function (req,res) {
+secure_app.get('*', function (req,res) {
     res.status(200).send('OK');
 });
 
@@ -45,6 +20,8 @@ const PORT = (process.env.PORT || 8081);
 secure_app.listen(PORT, () => {
   console.log(`Secure Server is running on port ${PORT}.`);
 });
+
+
 const VUE_PORT = (process.env.VUE_SERVER_PORT || 8080)
 app.listen(VUE_PORT, () => {
     console.log(`Local Server is running on port `, VUE_PORT);
